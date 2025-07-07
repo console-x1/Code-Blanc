32f9c1a103448c2779489bcd863c4a06ed7b2ceb
codeurMax32 <164027504+codeurMax32@users.noreply.github.com>
2025-07-07 16:01:50 +0200

    Logs Connections V1

--- public/js/function.js
+++ public/js/function.js
@@ -116,11 +116,10 @@ async function EncodeDecode(input) {
 
     if (isBinary) {
         const output = cleanInput.match(/[01]{8}/g).map(bin => String.fromCharCode(parseInt(bin, 2))).join('');
-        console.log(output)
         if (output === "home") {home()} else if (output === "constitution") {constitution()} else if (output === "education") {code_de_l_education()}
-        // return output
+        return output
     } else {
         const output = input.split('').map(char => char.charCodeAt(0).toString(2).padStart(8, '0')).join(' ');
-        // return output
+        return output
     }
 }
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const PUBLIC_DIR = path.join(__dirname, 'public');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(PUBLIC_DIR));
+app.use(express.json());
 
 app.get('/home',  (req, res) => {
     res.sendFile(path.join(PUBLIC_DIR, 'index.html'));
@@ -31,6 +32,53 @@ app.get('/',  (req, res) => {
 app.get('/index',  (req, res) => {
     res.sendFile(path.join(PUBLIC_DIR, 'index.html'));
 });
+
+function extractIPs(ipRaw) {
+  const ipv6 = ipRaw;
+  let ipv4 = '';
+
+  if (ipRaw.startsWith('::ffff:')) {
+    ipv4 = ipRaw.split('::ffff:')[1];
+  } else if (ipRaw.includes('.')) {
+    ipv4 = ipRaw;
+  }
+
+  return { ipv4, ipv6 };
+}
+
+app.get('/get-ip', (req, res) => {
+  const rawIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const { ipv4, ipv6 } = extractIPs(rawIp);
+  res.json({ ipv4, ipv6 });
+});
+app.get('/old-page', (req, res) => {
+  const Old_Page_Encode = req
+  res.json({ ipv4, ipv6 });
+});
+
+app.post('/log', (req, res) => {
+  const { ipv4, ipv6, ua, date, h, min, sec, cookies, old_Page } = req.body;
+
+  const logEntry = `
+\n------------------------------------------------------------------------------------------------\n
+IPv4 : ${ipv4}
+IPv6 : ${ipv6}
+UserAgent : ${ua}
+Date : ${date}
+Heure : ${h} h ${min} min ${sec} sec
+Cookies : ${cookies}
+Page : ${old_Page}`;
+
+  const logPath = path.join(__dirname, 'logs/connections.log');
+  fs.appendFile(logPath, logEntry, (err) => {
+    if (err) {
+      console.error('Erreur écriture log :', err);
+      return res.sendStatus(500);
+    }
+    res.sendStatus(200);
+  });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Serveur Node.js - Site Ensemble-pour-la-France lancé sur le port n°${PORT}`.blue);
